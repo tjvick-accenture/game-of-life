822a5078c61830a2cfcadfd8867f3b10e779ca94
Tyler Vick <vick.tyler@gmail.com>
2019-11-08 20:32:08 -0500

    Serve page with express

---
+++ javascript/source/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname + "/app/index.html")));
+app.listen(port, () => console.log(`App listening on port ${port}`));
